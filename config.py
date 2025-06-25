29260627fc24995b4e52ac8db2328c21fe8140d7
Ehtasham Ahmed <ehtashamahmed15@gmail.com>
2025-06-25 06:29:27 +0530

    Include config.py for deployment

---
+++ config.py
@@ -0,0 +1,24 @@
+import streamlit as st
+
+# Load Firebase config from secrets
+FIREBASE_CONFIG = {
+    "apiKey": st.secrets["firebase"]["api_key"],
+    "authDomain": st.secrets["firebase"]["auth_domain"],
+    "projectId": st.secrets["firebase"]["project_id"],
+    "storageBucket": st.secrets["firebase"]["storage_bucket"],
+    "messagingSenderId": st.secrets["firebase"]["messaging_sender_id"],
+    "appId": st.secrets["firebase"]["app_id"],
+    "databaseURL": ""
+}
+
+# Snowflake config
+SNOWFLAKE_CONFIG = {
+    "user": st.secrets["snowflake"]["user"],
+    "password": st.secrets["snowflake"]["password"],
+    "account": st.secrets["snowflake"]["account"],
+    "warehouse": st.secrets["snowflake"]["warehouse"],
+    "database": st.secrets["snowflake"]["database"],
+    "schema": st.secrets["snowflake"]["schema"],
+    "role": st.secrets["snowflake"].get("role", None),
+}
+
